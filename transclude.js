bfb3c0f10973552973be30a737a42b116d93e4d4
Justin Vencel <failpunk@gmail.com>
2016-03-22 00:30:15 -0700

    Update the transclusion example

--- transclude.js
+++ transclude.js
@@ -1,16 +1,27 @@
 angular.module('myApp', [])
-  .component('myDetails', {
-    transclude: {
-        'title': '?detailsTitle',
-        'body': '?detailsBody',
-        'footer': '?detailsFooter'
-    },
-    template: `
-        <div>
-            <h1 ng-transclude="title">Default Title Text</h1>
-            <p ng-transclude="body">Default Body Text</p>
-            <small ng-transclude="footer">Default Footer Text</small>
-        </div>
+    .component('myDetails', {
+        transclude: {
+            'title': '?detailsTitle',
+            'body': '?detailsBody',
+            'description': '?detailsDescription'
+        },
+        template: `
+                <div class="content">
+                    <div class="header" ng-transclude="title">
+                        Jane Doe
+                    </div>
+                    <div class="meta" ng-transclude="body">
+                        Unknown
+                    </div>
+                    <div class="description" ng-transclude="description">
+                        Someone didn't bother to override the default description text.
+                    </div>
+                </div>
+                <div class="extra content">
+                    <div class="ui two buttons">
+                        <div class="ui basic green button">Hell Yeah!</div>
+                        <div class="ui basic red button">Please No!</div>
+                    </div>
+                </div>
     `
-  });
-  
+    });
